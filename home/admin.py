a6252bdc1b630bee8181b1f1006f988a6935c1b2
domsq <Domsq@hotmail.com>
2022-05-20 11:46:18 +0000

    Register admin view for WelcomeMessage model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import WelcomeMessage
 
-# Register your models here.
+
+@admin.register(WelcomeMessage)
+class WelcomeMessageAdmin(admin.ModelAdmin):
+    """Admin view for WelcomeMessage"""
+    list_display = ('title', 'body')
+    search_fields = ('title', 'body')
