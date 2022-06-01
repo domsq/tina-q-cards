14e5301abdcdad0c7648b6e746d5158bcc9c4d81
domsq <Domsq@hotmail.com>
2022-06-01 13:31:29 +0000

    Install Crispy Forms and update requirements file

--- tina_q_cards/settings.py
+++ tina_q_cards/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'storages',
+    'crispy_forms',
     'home',
 ]
 
@@ -67,6 +68,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tina_q_cards.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -80,6 +83,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
