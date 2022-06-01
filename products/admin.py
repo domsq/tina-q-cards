75d186edfa5c50fa05ef491e2e878cb15b8f59f1
domsq <Domsq@hotmail.com>
2022-06-01 16:06:45 +0000

    Register admin views for Category and Product models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """ Admin view for Category """
+    list_display = (
+        'name',
+        'description',
+    )
+
+    search_fields = (
+        'name',
+        'description',
+    )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """ Admin view for Product """
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    search_fields = (
+        'name',
+        'category__name',
+    )
+
+    ordering = ('name',)
