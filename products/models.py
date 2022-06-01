e4f9fed66cbf8418b6749f33867470b3fda897a6
domsq <Domsq@hotmail.com>
2022-06-01 15:40:56 +0000

    Define Product model

--- products/models.py
+++ products/models.py
@@ -12,3 +12,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    """ Schema for the Product model """
+
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
