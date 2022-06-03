61a3b10ef9f64fea1a3e3afbe1ad0652394b20c8
domsq <Domsq@hotmail.com>
2022-06-03 11:51:46 +0000

    Add product sorting functionality

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Category, Product
 
 
@@ -11,10 +12,25 @@ def all_products(request):
     """
 
     categories = None
+    direction = None
     products = Product.objects.all()
     query = None
+    sort = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -26,11 +42,15 @@ def all_products(request):
                 messages.error(request, "You entered no search criteria!")
                 return redirect(reverse('products'))
 
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = (Q(name__icontains=query) |
+                       Q(description__icontains=query))
             products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'current_categories': categories,
+        'current_sorting': current_sorting,
         'products': products,
         'search_term': query,
     }
