69162b9f4c020c68fa1f877e2560decad1f0b21c
domsq <Domsq@hotmail.com>
2022-06-07 08:56:20 +0000

    Add basket adjust view and URL

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 def view_basket(request):
@@ -21,3 +21,18 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """ Adjust the quantity of the specific product in the basket """
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
