e6be45af7475822193a86529bfa246de1dc53bad
domsq <Domsq@hotmail.com>
2022-06-09 10:39:58 +0000

    Configure basic Stripe payment functionality

--- checkout/views.py
+++ checkout/views.py
@@ -25,7 +25,7 @@ def checkout(request):
     intent = stripe.PaymentIntent.create(
         amount=stripe_total,
         currency=settings.STRIPE_CURRENCY,
-    )    
+    )
 
     order_form = OrderForm()
 
