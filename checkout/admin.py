25681f7ec5ac220a81620c0ad31a214b840feeb7
domsq <Domsq@hotmail.com>
2022-06-13 15:15:21 +0000

    Update checkout views to use profile data for form

--- checkout/admin.py
+++ checkout/admin.py
@@ -3,18 +3,20 @@ from .models import Order, OrderLineItem
 
 
 class OrderLineItemAdminInline(admin.TabularInline):
+    """ Admin view for the OrderLineItem model """
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
+    """ Admin view for the OrderLineItem items """
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
                        'grand_total', 'original_basket', 'stripe_pid', )
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
