fbc7c2440954141f871084aa49123d814aafa1a1
domsq <Domsq@hotmail.com>
2022-06-14 15:31:28 +0000

    Update view, template and URL for blog app

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import BlogPost
+
+
+def view_blog(request):
+    """ View to render blog posts """
+
+    posts = BlogPost.objects.all()
+
+    template = 'blog/blog.html'
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, context, template)
