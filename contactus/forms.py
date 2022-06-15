c9a4a85b5a3065310f4560b05ab7bc68041e56f5
domsq <Domsq@hotmail.com>
2022-06-15 12:02:08 +0000

    Create model form for contact us

---
+++ contactus/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import ContactUs
+
+
+class ContactForm(forms.ModelForm):
+    """
+    Contact Us form
+    """
+    class Meta:
+        model = ContactUs
+        fields = ('name', 'email', 'subject', 'body',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Name',
+            'email': 'Email Address',
+            'subject': 'Subject',
+            'body': 'Message',
+        }
+
+        self.fields['name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].label = False
