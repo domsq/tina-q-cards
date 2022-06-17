e4040d24900f7be6f111a1367bf9091e4e67892d
domsq <Domsq@hotmail.com>
2022-06-17 19:58:18 +0000

    Update JS files following validation check

--- basket/static/basket/js/update-remove.js
+++ basket/static/basket/js/update-remove.js
@@ -2,17 +2,17 @@
  $('.update-link').click(function(e) {
     let form = $(this).prev('.update-form');
     form.submit();
-})
+});
 
 // Remove item and reload on click
 $('.remove-item').click(function(e) {
     let csrfToken = "{{ csrf_token }}";
     let itemId = $(this).attr('id').split('remove_')[1];    
     let url = `/basket/remove/${itemId}/`;
-    let data = {'csrfmiddlewaretoken': csrfToken}
+    let data = {'csrfmiddlewaretoken': csrfToken};
 
     $.post(url, data)
      .done(function() {
         location.reload();
      });
-})
+});
