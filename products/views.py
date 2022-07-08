3ee4244fbc473451e546895484ba64e1e370ca43
Dominic Quail <Domsq@hotmail.com>
2022-07-08 10:54:06 +0000

    Configure view, template, URL for product add

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Category, Product
+from .forms import ProductForm
 
 
 def all_products(request):
@@ -74,3 +75,14 @@ def product_details(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a new product """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
