536726dbd0454fa85c95de5d583b96c0f682b67d
Dominic Quail <Domsq@hotmail.com>
2022-07-08 16:42:09 +0000

    Create reply model and register admin view

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import BlogPost
+from .models import BlogPost, Reply
 
 
 @admin.register(BlogPost)
@@ -10,3 +10,11 @@ class BlogPostAdmin(SummernoteModelAdmin):
     search_fields = ('title', 'body')
     list_filter = ('title', 'created')
     summernote_fields = ('body')
+
+
+@admin.register(Reply)
+class ReplyAdmin(admin.ModelAdmin):
+    """Admin view for Replies"""
+    list_display = ('post', 'created', 'creator')
+    search_fields = ('body', 'creator__username')
+    list_filter = ('creator__username', 'created')
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class BlogPost(models.Model):
@@ -16,3 +17,23 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Reply(models.Model):
+    """Schema for the Reply model"""
+
+    class Meta:
+        verbose_name_plural = 'Replies'
+        """Sorts the posts in descending order"""
+        ordering = ["-created"]
+
+    creator = models.ForeignKey(User, on_delete=models.CASCADE,
+                                related_name="user_replies")
+    body = models.TextField()
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE,
+                             related_name="blog_replies")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Reply {self.body} by {self.creator}"
