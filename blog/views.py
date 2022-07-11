8330ee8b9ec45ada6f72a20ae17a643ed73714ee
Dominic Quail <Domsq@hotmail.com>
2022-07-11 14:31:56 +0000

    Configure blog reply functionality

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
 from .models import BlogPost
 from .forms import ReplyForm
 
@@ -22,6 +23,21 @@ def blog_detail(request, blog_id):
     queryset = BlogPost.objects
     post = get_object_or_404(queryset, pk=blog_id)
     replies = post.blog_replies.all()
+
+    if request.method == 'POST':
+        if not request.user.is_authenticated:
+            messages.error(request, 'Action not allowed')
+            return redirect(reverse('home'))
+        else:
+            reply_form = ReplyForm(request.POST)
+            if reply_form.is_valid():
+                reply_form.instance.creator = request.user
+                reply = reply_form.save(commit=False)
+                reply.post = post
+                reply.save()
+                messages.success(request, 'Reply successfully added!')
+            else:
+                messages.error(request, 'Reply not added!')
 
     form = ReplyForm()
     template = 'blog/blog_detail.html'
