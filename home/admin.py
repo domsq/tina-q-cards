76ee12106fb0ea36d813094c6a6197a082f0da1c
Dominic Quail <Domsq@hotmail.com>
2022-07-12 14:40:17 +0000

    Remove unneeded WelcomeMessage model

--- home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import WelcomeMessage
-
-
-@admin.register(WelcomeMessage)
-class WelcomeMessageAdmin(admin.ModelAdmin):
-    """Admin view for WelcomeMessage"""
-    list_display = ('title', 'body')
-    search_fields = ('title', 'body')
